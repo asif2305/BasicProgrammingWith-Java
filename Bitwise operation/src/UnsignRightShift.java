b71cec25f8ea54c13962ac6289bf02f674309d0c
asif2305 <mygithub2305@gmail.com>
2020-01-12 15:55:31 +0100

    change class name

--- Bitwise operation/src/UnsignRightShift.java
+++ Bitwise operation/src/UnsignRightShift.java
@@ -1,5 +1,5 @@
 
-public class UnsignRightShoft {
+public class UnsignRightShift {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- Enumerations/src/Enumerations.java
+++ Enumerations/src/Enumerations.java
@@ -1,4 +1,5 @@
-// A simplle 
+// A simplle  
+
 
 
 
