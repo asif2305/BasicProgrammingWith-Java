aac6f2f9b891761fd4639e396d2e4dee293bd6cf
asif2305 <mygithub2305@gmail.com>
2020-01-12 16:16:51 +0100

    modifying method

--- Enumerations/src/Enumerations.java
+++ Enumerations/src/Enumerations.java
@@ -1,12 +1,32 @@
-// A simplle  
+// A simple enum example where enum is declared outside any class
+//Enum declaration can be done outside a Class or inside a Class but not inside a Method. 
 
-
-
+enum ColorCheck
+{
+	RED,GREEN,BLUE,WHITE,BLACK;
+}
+enum Season
+{
+	WINTER(10),SPRING(20),SUMMER(30),FALL(40);
+	public int value;
+	private Season(int value)
+	{
+		this.value=value;
+	}
+}
 
 public class Enumerations {
 
+	// Drive Method
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ColorCheck color=ColorCheck.BLUE;
+		System.out.println(color);
+		
+		
+	   for(Season s: Season.values())
+	   {
+		   System.out.println(s + " " + s.value);
+	   }
 
 	}
 
