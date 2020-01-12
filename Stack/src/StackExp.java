ee6d6909e7e8d2ee1cac11d6f1a2804733c894af
asif2305 <mygithub2305@gmail.com>
2020-01-12 13:27:12 +0100

    adding stack_search() method

--- Stack/src/StackExp.java
+++ Stack/src/StackExp.java
@@ -13,18 +13,35 @@ public class StackExp {
 			stack.push(i*5);
 		}
 		System.out.println("Printing the stack value which is push:");
-		System.out.println(stack);
+		System.out.println("Push :"+stack +"\nsize of stack: "+ stack.size());
 	}
 	// Displaying the elements on the top of the stack
 	static void stack_peek(Stack<Integer> stack)
 	{
 		Integer element=stack.peek();
-		System.out.print("Element on stack top :" + element);
+		System.out.println("Element on stack top :" + element);
 	}
 	
-  // poping elements from the top of the stack
+  // Popping elements from the top of the stack
 	static void stack_pop(Stack<Integer> stack)
 	{
+		
+		System.out.print("Pop : ");
+		for(int i=0;i<5;i++)
+		{
+			Integer dataElement=(Integer)stack.pop();
+			System.out.print(" "+ dataElement);
+			
+		}
+		System.out.println();
+	}
+	// searching Elements in the stack
+	static void stack_search(Stack<Integer> stack,int element)
+	{
+		Integer positionOfStackElements=(Integer) stack.search(element);
+		if(positionOfStackElements==-1)
+			System.out.println("Element not found");
+		else System.out.println("Element is found at position :"+ positionOfStackElements);
 		
 	}
 	public static void main(String[] args) 
@@ -33,6 +50,10 @@ public class StackExp {
 		Stack<Integer> stack=new Stack<Integer>();
 		stackEmp.stackPush(stack);
 		stackEmp.stack_peek(stack);
+		stackEmp.stack_search(stack, 10);
+		stackEmp.stack_pop(stack);
+		
+		stackEmp.stack_search(stack, 4);
 	}
 
 }
